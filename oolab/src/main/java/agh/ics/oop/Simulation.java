2f562b5953cf8b657f34d201a5d4ba2804c6849d
Stanisław Brzozowski <grywam7@gmail.com>
2024-11-14 02:58:59 +0100

    more

--- oolab/src/main/java/agh/ics/oop/Simulation.java
+++ oolab/src/main/java/agh/ics/oop/Simulation.java
@@ -1,6 +1,6 @@
 package agh.ics.oop;
 
-import agh.ics.oop.model.*;
+import agh.ics.oop.model.*; //to pewnie jest źle, ale w sumie to czemu?
 
 import java.util.Collections;
 import java.util.ArrayList;
